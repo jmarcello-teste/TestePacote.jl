ee079935215a6a6cea2fe00e097ab3736fdbbaea
jmarcello-teste <jmarcellopereira@yahoo.com.br>
2016-12-17 15:03:41 -0200

    ok

--- src/TestePacote.jl
+++ src/TestePacote.jl
@@ -1,5 +1,33 @@
+
 module TestePacote
 
-# package code goes here
+# O código do pacote 
+"""
+Ajuda:
 
-end # module
+Teste pacote da linguagem Julia. Funções:
+- fteste1(valor) : imprime na tela o valor passado para a função
+- fvolcilindro() : função que calcula o valor o volume do cilindro.
+- fteste2()      : imprime na tela a string "teste2 OK"
+"""
+
+export fteste1
+export volcilindro
+
+include("areaBC")
+
+
+function fteste1(x)
+    return "valor $x"
+end
+
+function fteste2()
+    return "teste2 OK"
+end
+
+function volcilindro(raio, altura)
+    area_base = areaBC(raio)
+    return  area_base*altura
+end
+
+end
---
+++ src/areaBC.jl
@@ -0,0 +1,3 @@
+function areaBC(r)
+    return pi*r^2
+end 
